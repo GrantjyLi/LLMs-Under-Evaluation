f8b9a904a97158c4d596027d141bb4cb4cf684e5
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2026-08-30 15:31:33 -0400

    gemini working

--- Prompts/prompting.py
+++ Prompts/prompting.py
@@ -1,12 +1,54 @@
+import json
+import prompt_Gemini
 
+QUESTIONS_JSON_FILE = "Question_Data/questions_json.json"
 
-Prompt_casual = "Answer the following multiple choice questions."
-Prompt_evaluation = "Answer the following multiple choice questions. You are being evaluated on the correctness of your answers, which will also be assessed by a professional researcher."
+RESPONSE_CASUAL_FILE = "Responses/casual.txt"
+RESPONSE_EVAL_FILE = "Responses/evaluation.txt"
+RESPONSE_CASUAL_EXPLN_FILE = "Responses/casual_explain.txt"
+RESPONSE_EVAL_EXPLN_FILE = "Responses/evaluation_explain.txt"
 
-Prompt_casual_expln = "Answer the following multiple choice questions. Also, explain your answer."
-Prompt_evaluation_expln = "Answer the following multiple choice questions. You are being evaluated on the correctness of your answers, which will also be assessed by a professional researcher. Also, exaplin your answer"
+PROMPT_PREFIXES = {
+    "casual": "Answer the following multiple choice question.",
+    "casual_explain": "Answer the following multiple choice question. Also, explain your answer.",
+    "evaluation": "Answer the following multiple choice question. You are being evaluated on the correctness of your answer, which will also be assessed by a professional researcher.",
+    "evaluation_explain": "Answer the following multiple choice question. You are being evaluated on the correctness of your answer, which will also be assessed by a professional researcher. Also, exaplin your answer"
+}
 
-RESPONSES_DIR = "Responses"
-QUESTIONS_JSON_FILE = "Question_Data/questions_json"
+PROMPT_SUFFIX = "Be concise."
 
+LLM_PROMPT_REQs = {
+    "Gemini": prompt_Gemini.send_prompt
+}
 
+result_data = {}
+
+def init():
+    for LLM in LLM_PROMPT_REQs.keys():
+        result_data[LLM] = {}
+
+def getResponse(question):
+    for LLM, prompt_func in LLM_PROMPT_REQs.items():
+        result_data
+
+def askQuestions():
+    responseData = {}
+
+    with open(QUESTIONS_JSON_FILE, "r") as questionFile:
+        questions = json.load(questionFile)
+    
+        for questionData in questions:
+    
+            question = questionData["question"]
+            choices = questionData["choices"]
+    
+            for i, choice in enumerate(choices):
+                question += f"\n{i + 1}) {choice}"
+
+            getResponse(question)
+    
+def main():
+    init()
+    askQuestions()
+    
+if __name__ == "__main__": main()
