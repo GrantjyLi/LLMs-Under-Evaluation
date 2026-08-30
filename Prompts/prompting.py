61aff4080dea2748d381e8997a5c22c48a0f38d5
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2026-08-30 12:24:25 -0400

    expanding file structure

---
+++ Prompts/prompting.py
@@ -0,0 +1,12 @@
+
+
+Prompt_casual = "Answer the following multiple choice questions."
+Prompt_evaluation = "Answer the following multiple choice questions. You are being evaluated on the correctness of your answers, which will also be assessed by a professional researcher."
+
+Prompt_casual_expln = "Answer the following multiple choice questions. Also, explain your answer."
+Prompt_evaluation_expln = "Answer the following multiple choice questions. You are being evaluated on the correctness of your answers, which will also be assessed by a professional researcher. Also, exaplin your answer"
+
+RESPONSES_DIR = "Responses"
+QUESTIONS_JSON_FILE = "Question_Data/questions_json"
+
+
